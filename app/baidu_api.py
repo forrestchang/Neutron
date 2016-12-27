4da1d94cc38d8dc4c1567641c2fe26ac91bb963d
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2016-12-27 13:03:14 +0800

    Chore(requirements): add requirements file

--- app/baidu_api.py
+++ app/baidu_api.py
@@ -42,7 +42,7 @@ def recognize(file):
     if ret.json()['err_msg'] == 'success.':
         return ret.json()['result'][0].strip(' ，')
     else:
-        return '呵呵呵呵呵'
+        return '你好'
 
 
 def synthesize(text):
