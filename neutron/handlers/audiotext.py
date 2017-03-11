2fa1570c33f088935c3be3d84007cd82bcc987e2
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2017-03-11 17:16:55 +0800

    Docs(audiotext.py): add docstrings

--- neutron/handlers/audiotext.py
+++ neutron/handlers/audiotext.py
@@ -1,19 +1,34 @@
+"""Handler for audio to text service.
+
+"""
+
 from ..layers.baidu import BaiduService
 from . import load_configs
 import json
 
 
 class AudioTextHandler(object):
+    """Class for audio-text service.
+
+    """
 
     def __init__(self):
         configs = load_configs('voice')
         self.audio_text_service = BaiduService(**configs['baidu'])
 
     def audio2text(self, audio_file):
+        """Convert audio file to text.
+        Args:
+            audio_file (file stream)
+
+        Returns:
+            str: audio to text
+
+        """
         return self.audio_text_service.audio2text(audio_file)
 
     def text2audio(self, text):
-        """Convert text to audio file
+        """Convert text to audio file.
 
         params:
         -------
