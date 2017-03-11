94fa6ccdee70ff9e2768a761a097c6ab7c434157
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2017-03-11 17:33:41 +0800

    Docs(turing.py): add docstrings

--- neutron/layers/turing.py
+++ neutron/layers/turing.py
@@ -1,10 +1,13 @@
-import urllib
-import requests
-import json
+"""This module used for process requests from turing API.
+"""
+
 import uuid
+import requests
 
 
 class Turing_AI(object):
+    """Turing_AI service.
+    """
 
     def __init__(self, **kwargs):
         self.url = 'http://www.tuling123.com/openapi/api'
@@ -12,6 +15,14 @@ class Turing_AI(object):
         self.userid = uuid.uuid1().hex
 
     def get_response(self, msg):
+        """Get response from http://www.tuling123.com/openapi/api
+
+        Args:
+            msg (str): message you want to use Turing_AI API to process.
+
+        Returns:
+            str: result after processing text you sent by Turing_AI
+        """
         msg_json = {
             "key": self.access_key,
             "info": msg,
