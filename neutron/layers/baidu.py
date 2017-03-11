da675958daa78b573c237d3ab27c48157626f815
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2017-03-11 17:28:32 +0800

    Docs(baidu.py): add docstrings

--- neutron/layers/baidu.py
+++ neutron/layers/baidu.py
@@ -1,7 +1,10 @@
-import requests
+"""This module used for process requests from baidu API.
+"""
+
 import urllib.parse
 import time
 import uuid
+import requests
 
 
 class BaiduService(object):
@@ -19,10 +22,14 @@ class BaiduService(object):
         self.token = self.gen_token()
 
     def gen_token(self):
+        """Generate access token use self.issue_token()
+        """
         new_token = self.issue_token()
         return new_token
 
     def issue_token(self):
+        """Get token from https://openapi.baidu.com/oauth/2.0/token
+        """
         resp = requests.post(
             self.issue_token_url,
             data={
@@ -34,6 +41,16 @@ class BaiduService(object):
         return resp.json()['access_token']
 
     def audio2text(self, audio_file, lan='zh'):
+        """Get text from http://vop.baidu.com/server_api
+
+        Args:
+            audio_file: .wav file
+            lan (str): which language you sent to the server, default is 'zh'
+
+        Returns:
+            str: text converted from audio_file
+
+        """
         headers = {
             "Content-Type": "audio/wav;rate=8000"
         }
@@ -54,6 +71,20 @@ class BaiduService(object):
             return 'fail'
 
     def text2audio(self, text, lan='zh', ctp=1, spd=3, pit=3, vol=9, per=0):
+        """Get audio from http://tsn.baidu.com/text2audio
+
+        Args:
+            text (str): text you want to convert
+            lan (str): the text language, default is 'zh'
+            ctp (int):
+            spd (int): voice speed
+            pit (int):
+            vol (int): voice volum
+            per (int):
+
+        Returns:
+            BytesIO: .mp3 file converted from text
+        """
         parms = {
             "tex": text,
             "lan": lan,
